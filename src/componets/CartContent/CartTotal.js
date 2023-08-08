4190680046ff69193ee740e8466b279f53576245
cesarrodriguez26 <124197857+cesarrodriguez26@users.noreply.github.com>
2023-08-08 21:34:15 +0200

    re-entrega react

--- src/componets/CartContent/CartTotal.js
+++ src/componets/CartContent/CartTotal.js
@@ -1,18 +1,41 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { dataContext } from "../Context/DataContext";
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('tu_clave_publica');
 
 const CartTotal = () => {
-  
-  const { cart } = useContext(dataContext);
+  const { cart, clearCart } = useContext(dataContext); 
 
-  const total = cart.reduce((acc,el)=> acc + el.Price * el.quanty, 0);
-  
-    return(
-     <div className='cartTotal'>
-        <h3>Total a pagar: {total} $</h3>
-        <button>Pagar</button>
-    </div>
-    );
+  const total = cart.reduce((acc, el) => acc + el.Price * el.quanty, 0);
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const { error } = await stripe.redirectToCheckout({
+        lineItems: [{ price: 'precio_123', quantity: 1 }], // Reemplaza 'precio_123' con el ID de tu producto/precio en Stripe
+        mode: 'payment',
+        successUrl: 'https://tudominio.com/success', // URL a la que redirigir al usuario después de un pago exitoso
+        cancelUrl: 'https://tudominio.com/cancel', // URL a la que redirigir al usuario si cancela el pago
+      });
+
+      if (error) {
+        console.error("Error al procesar el pago:", error);
+      } else {
+        console.log("Pago procesado exitosamente.");
+        clearCart();
+      }
+    } catch (error) {
+      console.error("Error al procesar el pago:", error);
+    }
   };
+
+  return (
+    <div className="cartTotal">
+      <h3>Total a pagar: {total} $</h3>
+      <button onClick={handlePayment}>Pagar</button>
+    </div>
+  );
+};
 
 export default CartTotal;
